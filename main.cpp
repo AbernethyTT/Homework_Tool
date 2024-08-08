4c516ecba29e4a31f9f31e617248c65a26b54c4b
AS_IX <abernethyTT@outlook.com>
2024-08-08 10:32:00 +0800

    a
    a

--- main.cpp
+++ main.cpp
@@ -3,20 +3,18 @@ using namespace std;
 int main()
 {
 	char chose;
-	char user_a = 1;
-	char user_s= 2;
 AS:
 	cout << "Start enter '1'" << endl;
 	cout << "Dont start ener '2'" << endl;
-	cout << "Please enter 1 or 2 :";
+	cout << "Please enter '1' or '2' :";
 	cin >> chose;
-	if (chose =1)
+	if (chose =='1')
 	{
 		cout << "start";
 	}
-	if (chose =2)
+	else if (chose =='2')
 	{
-		cout << "d";
+		cout << "Don't start";
 	}
 	 else {
 		 goto AS;
